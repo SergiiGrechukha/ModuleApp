ad33394070555e186253655f0016d6a68bf1fb6d
sergii.grechukha <sergii.grechukha@grabtaxi.com>
2019-11-22 14:35:22 +0800

    Fixed streams compile error

--- app-shell/src/streamD/java/routing/StreamRouting.kt
+++ app-shell/src/streamD/java/routing/StreamRouting.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.Toast
-import com.loki.astream.AStreamActivity
 import com.loki.core.routing.Router
 
 class StreamRouting : Router {
@@ -12,7 +11,8 @@ class StreamRouting : Router {
     val TAG = "ROUTER"
 
     override fun launchStreamA(context: Context) {
-        context.startActivity(Intent(context, AStreamActivity::class.java))
+        Log.d(TAG, "Stub this action")
+        Toast.makeText(context, "Stub this action", Toast.LENGTH_SHORT).show()
     }
 
     override fun launchStreamB(context: Context) {
